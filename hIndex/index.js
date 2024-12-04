669eb89dcf90da460fcd1cda7c1759ad10515591
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-12-04 12:07:53 +0300

    feat:Add hIndex

---
+++ hIndex/index.js
@@ -0,0 +1,10 @@
+function hIndex(citations) {
+  citations.sort((a, b) => b - a);
+  let count = 0;
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= count + 1) {
+      count++;
+    }
+  }
+  return count;
+}
