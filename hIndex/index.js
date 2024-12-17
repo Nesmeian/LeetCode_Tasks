a7f557fe74b7f7b077c7d47458f6b0a6662509fa
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-12-17 18:46:07 +0300

    check

--- hIndex/index.js
+++ hIndex/index.js
@@ -3,7 +3,7 @@ function hIndex(citations) {
   let count = 0;
   for (let i = 0; i < citations.length; i++) {
     if (citations[i] >= count + 1) {
-      count++;
+      count+=1;
     }
   }
   return count;
